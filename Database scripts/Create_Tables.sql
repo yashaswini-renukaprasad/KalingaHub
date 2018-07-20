b4038cac85828644e4ff47aaf7ff2052e381fd42
Yashaswini <yashaswini.renukaprasad@mindtree.com>
2018-07-20 14:39:48 +0530

    Base code structure

---
+++ Database scripts/Create_Tables.sql
@@ -0,0 +1,66 @@
+
+
+CREATE TABLE [dbo].[Category] (
+    [Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR (100)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+------------------------------------------------------------------------
+
+CREATE TABLE [dbo].[Tag] (
+    [Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR (100)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+-----------------------------------------------------------------------
+
+CREATE TABLE [dbo].[User] (
+    [Id]  UNIQUEIDENTIFIER NOT NULL,
+    [Mid] NVARCHAR(10)      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+------------------------------------------------------------------------
+
+CREATE TABLE [dbo].[Question] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [IsActive]     BIT              DEFAULT 1 NOT NULL,
+    [CreatedBy]    UNIQUEIDENTIFIER     NOT NULL,
+    [CreatedDate]  DATETIME             NOT NULL DEFAULT GETDATE(),
+    [ModifiedDate] DATETIME             NULL DEFAULT NULL,
+    [Title]        NVARCHAR (150)     NOT NULL,
+    [Description]  NVARCHAR(MAX)    NOT NULL,
+    [CategoryId]   UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+---------------------------------------------------------------------
+
+CREATE TABLE [dbo].[Answer] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [QID]          UNIQUEIDENTIFIER NOT NULL,
+    [Description]  NVARCHAR(MAX)    NOT NULL,
+    [CreatedDate]  DATETIME             NOT NULL DEFAULT GETDATE(),
+    [ModifiedDate] DATETIME             NULL DEFAULT NULL,
+    [CreatedBy]    UNIQUEIDENTIFIER     NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+------------------------------------------------------------------------
+
+CREATE TABLE [dbo].[QuestionTags] (
+    [Id]    UNIQUEIDENTIFIER NOT NULL,
+    [TagId] UNIQUEIDENTIFIER NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+-----------------------------------------------------------------------
+
+CREATE TABLE [dbo].[Upvote] (
+    [Id]     UNIQUEIDENTIFIER NOT NULL,
+    [QuestionId]    UNIQUEIDENTIFIER NOT NULL,
+    [UserId] UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
